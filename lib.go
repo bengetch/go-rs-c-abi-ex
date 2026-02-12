96b9a28e61efd2994b167df5f4ce2d4e2b3f134e
bengetch <bengetch@gmail.com>
2026-02-12 10:22:06 -0500

    path fix

--- lib.go
+++ lib.go
@@ -1,10 +1,10 @@
 package go_rs_c_abi_ex
 
 /*
-#cgo darwin,amd64 LDFLAGS: ${SRCDIR}/prebuilt/x86_64-apple-darwin/libselector_lib.a
-#cgo darwin,arm64 LDFLAGS: ${SRCDIR}/prebuilt/aarch64-apple-darwin/libselector_lib.a
-#cgo linux,amd64  LDFLAGS: ${SRCDIR}/prebuilt/x86_64-unknown-linux-gnu/libselector_lib.a
-#cgo linux,arm64  LDFLAGS: ${SRCDIR}/prebuilt/aarch64-unknown-linux-gnu/libselector_lib.a
+#cgo darwin,amd64 LDFLAGS: ${SRCDIR}/prebuilt/x86_64-apple-darwin/librs_c_abi_ex.a
+#cgo darwin,arm64 LDFLAGS: ${SRCDIR}/prebuilt/aarch64-apple-darwin/librs_c_abi_ex.a
+#cgo linux,amd64  LDFLAGS: ${SRCDIR}/prebuilt/x86_64-unknown-linux-gnu/librs_c_abi_ex.a
+#cgo linux,arm64  LDFLAGS: ${SRCDIR}/prebuilt/aarch64-unknown-linux-gnu/librs_c_abi_ex.a
 
 #include <stdint.h>
 #include <stdbool.h>
